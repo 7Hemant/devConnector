0af74c968fc43aa2474f1f2b1bdab6ba5a47a02f
7hemant <hemantmourya127@gmail.com>
2023-01-10 22:16:54 +0530

    mongodb setup

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
 const ConnectedDB = require("./config/db");
+
+mongoose.set("strictQuery", true);
 const PORT = process.env.PORT;
 
 const app = express();
