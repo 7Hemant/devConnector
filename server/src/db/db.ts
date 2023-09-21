8680b282e5586ea004349b73a0a621afb04c7fe4
HEMANT.MOURYA <hemant.mourya@cloudactivelabs.com>
2023-09-21 22:45:18 +0530

    db connection setup is done

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -3,7 +3,7 @@ import Mongoose from "mongoose";
 const ConnectedDB = async () => {
   try {
     const connected = await Mongoose.connect(process.env.MONGO_URL || " ");
-    console.log(` conneted to mongodb${connected.connection.host}`);
+    console.log(` conneted to mongodb ${connected.connection.host}`);
   } catch (error) {
     console.error(error);
   }
